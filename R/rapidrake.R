8bf08927de454ffcda9511551a1e623cc5b2efab
Chris Baker <chriscrewbaker@gmail.com>
2017-08-03 00:45:42 -0400

    Add remaining r package files

--- R/rapidrake.R
+++ R/rapidrake.R
@@ -1,19 +1,15 @@
 #' Rapid RAKE
 #'
-#' A relatively fast version of the Rapid Automatic Keyword Extraction (RAKE) algorithm. See \href{Automatic keyword extraction from individual documents}{http://media.wiley.com/product_data/excerpt/22/04707498/0470749822.pdf} for details on how RAKE works.
+#' A relatively fast version of the Rapid Automatic Keyword Extraction (RAKE) algorithm. See \href{http://media.wiley.com/product_data/excerpt/22/04707498/0470749822.pdf}{Automatic keyword extraction from individual documents} for details on how RAKE works.
 #'
-#' @param txt
-#' @param stop_words
-#' @param word_min_char
-#' @param stem
-#' @param keep_pos
-#'
-#' @return
+#' @inheritParams slowraker::slowrake
 #' @export
 #'
 #' @examples
 rapidrake <- function(txt, stop_words = slowraker::smart_words,
-                      word_min_char = 3, stem = TRUE,
-                      keep_pos = c("NN", "NNS", "NNP", "NNPS")) {
-
+                      filter_pos = c("VB", "VBD", "VBG", "VBN", "VBP", "VBZ"),
+                      word_min_char = 3, stem = TRUE) {
+  rkdr <- rJava::.jnew("org.crew102.rapidrake.caller.RakeDriver", txt,
+                      stop_words, filter_pos, word_min_char, stem)
+  rJava::.jcall(rkdr, "[<Keyword>", "drive")
 }
---
+++ inst/make-helpers.R
@@ -0,0 +1,8 @@
+# inheritParams has trouble getting this roxygen comment correct, so have to
+# manually overrride .Rd file
+
+mnfile <- file("man/rapidrake.Rd")
+rrrd <- readLines(mnfile)
+new_rrrd <- gsub("\\Q\\href{{https://rdrr.io/rforge/tm/man/stopwords.html}{tm::stopwords('SMART')}}\\E", "\\\\href{https://rdrr.io/rforge/tm/man/stopwords.html}{tm::stopwords('SMART')}", rrrd)
+writeLines(new_rrrd, mnfile)
+close(mnfile)
