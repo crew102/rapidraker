031fe14c6fe3d96f204007d130100f52b0fb8325
Chris Baker <chriscrewbaker@gmail.com>
2017-10-06 17:42:53 -0400

    Fixup build process

--- R/on-load.R
+++ R/on-load.R
@@ -1,3 +1,5 @@
 .onLoad <- function(libname, pkgname) {
   rJava::.jpackage(pkgname, lib.loc = libname)
+  ## drop this after dev:
+  rJava::.jaddClassPath(dir(file.path(getwd(), "inst/java"), full.names = TRUE))
 }
