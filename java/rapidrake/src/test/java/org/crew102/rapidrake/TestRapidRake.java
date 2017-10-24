141e7481d5cc4eba906349a87bfddf300b588e2c
Chris Baker <chriscrewbaker@gmail.com>
2017-10-23 21:49:52 -0400

    add example test case

---
+++ java/rapidrake/src/test/java/org/crew102/rapidrake/TestRapidRake.java
@@ -0,0 +1,38 @@
+package org.crew102.rapidrake;
+
+import static org.junit.Assert.*;
+import org.crew102.rapidrake.model.*;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestRapidRake {
+	
+	RakeAlgorithm myAlg;
+	Document myDoc;
+	
+	@Before
+	public void setUp() throws java.io.IOException {
+		 
+			String[] stopWords = {"i", "do", "be"};
+			String[] stopPOS = {"VB", "VBD", "VBG", "VBN", "VBP", "VBZ"};
+			int wordMinChar = 2;
+			boolean stem = true;
+			RakeParams parms = new RakeParams(stopWords, stopPOS, wordMinChar, stem);
+					
+			myAlg = new RakeAlgorithm(parms, "model-bin/en-pos-maxent.bin");
+			myDoc = new Document("this is some text. that has some keywords");
+	}
+	
+    @Test
+    public void tesInitTokens() {
+    	Result myRes = myAlg.rake(myDoc);
+    	float[] someScores = myRes.getScores();
+    	double actual = (double) someScores[1];
+    	
+    	double expected = (double) 1.2;
+    	
+    	assertEquals(expected, actual, 0);
+     }
+   
+
+}
