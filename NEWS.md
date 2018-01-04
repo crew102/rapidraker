144dc62c7294e730f13f6a4603fd6f53d5fcb543
Chris Baker <chriscrewbaker@gmail.com>
2018-01-04 11:56:11 -0500

    get ready for cran

--- NEWS.md
+++ NEWS.md
@@ -1,1 +1,5 @@
 # rapidraker 0.1.0
+
+#### New functions
+
+* `rapidrake()` added to (rapidly) extract keywords from text using the RAKE algorithm
--- R/on-load.R
+++ R/on-load.R
@@ -1,5 +1,3 @@
 .onLoad <- function(libname, pkgname) {
   rJava::.jpackage(pkgname, lib.loc = libname)
-  ### need to drop this line for cran:
-  rJava::.jaddClassPath(list.files('inst/java', full.names = TRUE))
 }
