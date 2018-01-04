0402c126fbb7070a5e679bd0479f11849bd61dc4
chris baker <chriscrewbaker@gmail.com>
2018-01-04 15:36:25 -0500

    drop classpath handling script

--- inst/make-classpath-helpers.R
+++
@@ -1,9 +0,0 @@
-# Having rJava::.jaddClassPath(list.files('inst/java', full.names = TRUE))
-# inside .onLoad() allows testthat to find classes when testing. However,
-# we need to remove this line whenever pushing to cran.
-
-mnfile <- file("R/on-load.R")
-lines <- readLines(mnfile)
-lines[4] <- paste0("#", lines[4])
-writeLines(lines, mnfile)
-close(mnfile)
